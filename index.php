0766d9f0bf8ebd36d96bc477dca2fd17f38988ab
trucmuche <le.prince.du.81@hotmail.fr>
2018-10-19 15:42:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Bootstrap core CSS -->
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="./css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="./css/style.css" rel="stylesheet">
+</head>
+
+<body background="img/background1.jpg">
+
+
+
+  <!-- Start your project here-->
+
+  <!-- Card deck 1 -->
+<div class="card-deck" style="padding:5% 5% 5% 5% !important">
+  <!-- Card -->
+  <div class="card testimonial-card">
+
+    <!-- Background color -->
+    <div class="card-up lighten1">
+    </div>
+    <!-- Avatar -->
+    <div class="mx-auto">
+      <a href="views/guide.html">
+      <img src="img/img_guide.png"  alt="guide">
+      </a>
+    </div>
+
+    <!-- Content -->
+    <div class="card-body">
+      <!-- Name -->
+      <h4 class="card-title">Guide</h4>
+    </div>
+
+  </div>
+  <!-- Card -->
+
+  <!-- Card -->
+<div class="card testimonial-card">
+  <!-- Background color -->
+  <div class="card-up lighten1"></div>
+  <!-- Avatar -->
+  <div class="mx-auto">
+    <a href="views/rendre.html">
+    <img src="img/img_rendre.png"  alt="guide">
+    </a>
+  </div>
+
+  <!-- Content -->
+  <div class="card-body">
+    <!-- Name -->
+    <h4 class="card-title">Rendre un livre</h4>
+  </div>
+</div>
+</div>
+
+
+
+<!-- Card deck 1 -->
+<div class="card-deck"style="padding:0% 5% 5% 5% !important">
+
+  <!-- Card -->
+  <div class="card testimonial-card">
+    <!-- Background color -->
+    <div class="card-up lighten1"></div>
+    <!-- Avatar -->
+    <div class="mx-auto">
+      <a href="views/questions.html">
+      <img src="img/img_question.png"  alt="guide">
+    </a>
+    </div>
+
+    <!-- Content -->
+    <div class="card-body">
+      <!-- Name -->
+      <h4 class="card-title">Questions</h4>
+    </div>
+  </div>
+  <!-- Card -->
+
+  <!-- Card -->
+<div class="card testimonial-card">
+  <!-- Background color -->
+  <div class="card-up lighten1"></div>
+  <!-- Avatar -->
+  <div class="mx-auto">
+    <a href="views/aide.html">
+    <img src="img/img_aide.png"  alt="guide">
+  </a>
+  </div>
+
+  <!-- Content -->
+  <div class="card-body">
+    <!-- Name -->
+    <h4 class="card-title">Aide</h4>
+  </div>
+</div>
+
+  <!-- /Start your project here-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="./js/jquery-3.3.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="./js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="./js/mdb.min.js"></script>
+</body>
+
+</html>
